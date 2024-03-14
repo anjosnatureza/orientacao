0f45a7cabe17b2cb1529a5e8731688be2e2c8fa6
anjosnatureza <162371855+anjosnatureza@users.noreply.github.com>
2024-03-14 18:29:10 -0300

    Autalização título no lado esquerdo da página.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const links = document.querySelectorAll('.nav-link, .navbar-brand');
+    const links = document.querySelectorAll(".nav-link, .navbar-brand");
 
-    links.forEach(link => {
+    links.forEach((link) => {
         link.addEventListener("click", function (event) {
             event.preventDefault();
-            const headerHeight = document.querySelector('.navbar').offsetHeight;
-            const sectionId = link.getAttribute('href');
+            const headerHeight = document.querySelector(".navbar").offsetHeight;
+            const sectionId = link.getAttribute("href");
             let section;
-            if (sectionId === '#') {
+            if (sectionId === "#") {
                 section = document.documentElement; // Scroll to the top of the page
             } else {
                 section = document.querySelector(sectionId); // Scroll to the section
@@ -16,18 +16,18 @@ document.addEventListener("DOMContentLoaded", function () {
                 const sectionPosition = section === document.documentElement ? 0 : section.offsetTop - headerHeight;
                 window.scrollTo({
                     top: sectionPosition,
-                    behavior: "smooth"
+                    behavior: "smooth",
                 });
             }
 
             // Check if the menu is in responsive mode and close it
-            const navbarToggler = document.querySelector('.navbar-toggler');
-            if (navbarToggler && navbarToggler.getAttribute('aria-expanded') === 'true') {
+            const navbarToggler = document.querySelector(".navbar-toggler");
+            if (navbarToggler && navbarToggler.getAttribute("aria-expanded") === "true") {
                 navbarToggler.click(); // Close the responsive menu
             }
         });
     });
 
     // Initialize carousel
-    $('.carousel').carousel();
+    $(".carousel").carousel();
 });
